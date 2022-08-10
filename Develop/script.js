9a78a82fd7673a78468fece0d4711dfae473e9db
Jenny Bloemen <jennyjbloemen@gmail.com>
2022-08-10 13:37:24 -0600

    got start button working with timer

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,98 @@
-// Challenge
+var start = document.getElementById("start");
+var quiz = document.getElementById("questions");
+var choiceOne = documet.getElementById("one")
+var choiceTwo = documet.getElementById("two")
+var choiceThree = documet.getElementById("three")
+var choiceFour = documet.getElementById("four")
+var scoreDiv = document.getElementById("scoreContainer");
 
-var scoreDisplay = document.querySelector(".score");
-var highscoredisplay = document.querySelector(".high-score");
+var myQuestions = [
+  {
+    question: "Select the pair that encloses a JavaScript object literal?",
+    choiceOne: "{}", 
+    choiceTwo: "::", 
+    choiceThree: "()",
+    choiceFour: "[]",
+    answer: "three"
+  },
+  {
+    question: "Which one is a looping structure in JavaScript?",
+    choiceOne: "For",
+    choiceTwo: "While",
+    choiceThree: "do-while loops",
+    choiceFour: "All the above",
+    answer: "four"
+  },
+  {
+    question: "What is the correct way to call the random method on the Math global object?",
+    choiceOne: "math.random()", 
+    choiceTwo: "Math.random()", 
+    choiceThree: "random.Math()", 
+    choiceFour: "Math(random)",
+    answer: "two"
+  },
+  {
+    question: "Which of the following is a JavaScript Data Type?",
+    choiceOne: "Number", 
+    choiceTwo: "String", 
+    choiceThree: "Boolean", 
+    choiceFour: "Object",
+    answer: "All the above"
+  },
+  {
+    question: "What are the types of Pop up boxes available in JavaScript?",
+    choiceOne:"Function", 
+    choiceTwo: "Count", 
+    choiceThree: "Confirm", 
+    choiceFour: "Object",
+    answer: "Confirm"
+  }
+]
+
+var quizzboxEl = document.querySelectorAll("containerQuizzBox");
+
+var choicesEL = document.querySelectorAll("answerChoices");
+
+var myQuestionIndex = 0;
+// /* /* need to tie in My questions array*/
+// function populateQuestion () {
+//   questionsEl = myQuestions[]
+//   quizzboxEl.appendChild(textContent);
+//   console.log(question);
+//   for (let i = 0; i < myQuestions.length; i++);
+// }
+function populateQuestion() {
+  var questionDisplay = question[myQuestionIndex];
+  var questionsEl = document.getElementById("questions");
+  questionsEl.textContent = questionDisplay.question;
+}
+  
+  
+  
+  questionsEl.textContect = currentQuestion.title;
+  
+  var titleEl = document.getElementById("question-title");
+  titleEl.textContent = currentQuestion.title;
+
+  // clear out any old question choices
+  choicesEl.innerHTML = "";
 
 
 
-// Timer Code Block needs to start when start button is clicked
-var timeEl = document.querySelector(".time");
+
+
+function populateChoices () {
+  choicesEL.textContent = ("answerChoices");
+  quizzboxEl.appendChild(textContent);
+  console.log(choices);
+  
+// }
+
+
+
+// Timer Code Block that starts when the start quizz button is selected
 var mainEl = document.getElementById("time-time");
+var timeEl = document.querySelector(".time");
 var secondsLeft = 5;
 
 function setTime() {
@@ -23,108 +108,20 @@ function setTime() {
 }
 function sendMessage() {
   timeEl.textContent = "Your time has burned out!🔥Try again!";
-   mainEl.appendChild(textContent);
+  mainEl.appendChild(textContent);
 }
 // calls the timer function, I think this needs to call the time when the start button is clicked
-setTime();
 
+var startBtn = document.querySelector("#start");
+startBtn.addEventListener("click",startTimer); 
 
-//Building Quizz https://www.sitepoint.com/simple-javascript-quiz/
-
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
-
-function buildQuiz(){
-  const output = [];
-  myQuestions.forEach (
-    (currentQuestion, questionNumber) => {
-      
-      const answers = [];
-      for(letter in currentQuestion.choices){
-       
-        answers.push(
-          `<label>
-            <input type="radio" name="question${questionNumber}" value="${letter}">
-            ${letter} :
-            ${currentQuestion.choices[letter]}
-          </label>`
-        );
-      }
-
-      // add this question and its answers to the output
-      output.push(
-        `<div class="question"> ${currentQuestion.question} </div>
-        <div class="answers"> ${answers.join('')} </div>`
-      );
-    }
-  );
-
-  // finally combine our output list into one string of HTML and put it on the page
-  quizContainer.innerHTML = output.join('');
+function startTimer() {
+  console.log("start");
+  setTime ();
+  // clearInterval ();
 }
-  )
-function showResults(){}
 
-// display quiz right away
-buildQuiz();
 
-// on submit, show results
-submitButton.addEventListener('click', showResults);
-
-// Quiz Questions
-var myQuestions = [
-    {
-      question: "Select the pair that encloses a JavaScript object literal?",
-      choices: {
-        a: "{}", 
-        b: "::", 
-        c: "()", 
-        d:"[]"
-      },
-      answer: "c"
-    },
-    {
-      question: "Which one is a looping structure in JavaScript?",
-      choices: {
-        a:"For", 
-        b:"While", 
-        c:"do-while loops",
-        d:"All the above"
-      },
-      answer: "d"
-    },
-    {
-      question: "What is the correct way to call the random method on the Math global object?",
-      choices: {
-        a:"math.random()", 
-        b:"Math.random()", 
-        c:"random.Math()", 
-        d:"Math(random)"
-      },
-      answer: "Math.random()"
-    },
-    {
-      question: "Which of the following is a JavaScript Data Type?",
-      choices: {
-        a:"Number", 
-        b:"String", 
-        c:"Boolean", 
-        d:"Object"
-      },
-      answer: "All the above"
-    },
-    {
-      question: "What are the types of Pop up boxes available in JavaScript?",
-      choices: {
-        a:"Function", 
-        b:"Count", 
-        c:"Confirm", 
-        d:"Object"
-      },
-      answer: "Confirm"
-    }
-];
 
 
 // //example from class activity 7 not to be used, just for reference
@@ -144,14 +141,14 @@ var myQuestions = [
 // }
 
 // my attempt to create the same, but I am not sure if I need to create a second element. TT ta for clarity.
-if (answer = true) {
-  answer.textContent = "Answer is correct!"
-  //  use a for loop ++ to log correct answer? 
-} else if (answer = false) {
-  answer. textcontent = "Answer is incorrect!"
-//  use a for loop ++ to log wrong answer?
-document.body.appendChild(tag);
-}
+// if (answer = true) {
+//   answer.textContent = "Answer is correct!"
+//   //  use a for loop ++ to log correct answer? 
+// } else if (answer = false) {
+//   answer. textcontent = "Answer is incorrect!"
+// //  use a for loop ++ to log wrong answer?
+// document.body.appendChild(tag);
+// }
 
 
 // start button may need a event.preventDefault();

dd7cf4b09c176e1e72759e5238c28169950faba2
Jenny Bloemen <jennyjbloemen@gmail.com>
2022-08-15 13:16:10 -0600

    worked with ta and another student to start score and initials code, got enq quiz working

--- Develop/script.js
+++ Develop/script.js
@@ -6,14 +6,16 @@ var choiceOne = document.getElementById("one");
 var choiceTwo = document.getElementById("two");
 var choiceThree = document.getElementById("three");
 var choiceFour = document.getElementById("four");
+var intials = document.querySelector(".end-screen");
+var timerInterval;
+var finalScore = document.getElementById("final-score");
+
+
 // var scoreDiv = document.getElementById("scoreContainer");
 // var score = document.getElementById("score");
 // var highscore = document.getElementById("highScore");
 // var leaderBoard = '';
-
-// var submitBtn = document.querySelector(".submit");
 // var highscoreSection = document.querySelector(".high-scores");
-
 // Quetions, Choices and Answer Array
 var myQuestions = [
   {
@@ -50,7 +52,7 @@ var myQuestions = [
   },
   {
     question: "What are the types of Pop up boxes available in JavaScript?",
-    choiceOne:"1. Function", 
+    choiceOne: "1. Script", 
     choiceTwo: "2. Count", 
     choiceThree: "3. Confirm", 
     choiceFour: "4. Object",
@@ -65,8 +67,7 @@ var correct = 0;
 function populateQuestion() {
   var q = myQuestions[runningQindex];
   questionsEl.innerHTML = "<div list-group>" + q.question + "</div>";
-  questionsEl.innerHTML = q.question;
-  choiceOne.innerHTML =  q.question;
+  // questionsEl.innerHTML = q.question;
   choiceOne.innerHTML = q.choiceOne;
   choiceTwo.innerHTML = q.choiceTwo;
   choiceThree.innerHTML = q.choiceThree;
@@ -74,43 +75,109 @@ function populateQuestion() {
   }
   
   function checkAnswer(answer) {
-    if (myQuestions[runningQindex].correct == answer){
-    correct++
-      } else {
-        // How do I keep this from subtracting below 0
+    if (myQuestions[runningQindex].answer == answer){
+    correct++;
+    console.log(correct);
+    } else {
         secondsLeft = secondsLeft-5;
-        console.log("lost time")
-    
-    }
-    if (runningQindex< lastQindex) {
+        console.log("lost-time");
+     }
+    if (runningQindex < lastQindex) {
       runningQindex++;
       populateQuestion();
+    } else { 
+      (runningQindex > lastQindex)
+      quizEnd ();
     }
   }
-    
+// function answerIsCorrect (){
+ 
 var mainEl = document.getElementById("time-time");
 var timeEl = document.querySelector(".time");
-var secondsLeft = 15;
+var secondsLeft = 35;
 
 function setTime() {
-  var timerInterval = setInterval(function() {
+  timerInterval = setInterval(() => {
     secondsLeft--;
     timeEl.textContent = 'Timer: ' + secondsLeft + ' seconds';
-
-    if(secondsLeft <= 0) {
+    
+    // if(secondsLeft === 0) {
+    //   clearInterval(timerInterval);
+    //   sendMessage();
+    // }
+    // if(timerInterval === 0 || runningQindex === myQuestions.length){
+    //   clearInterval(timerInterval);
+// check if user ran out of time
+  if (timerInterval <= 0) {
       clearInterval(timerInterval);
-      sendMessage();
-      secondsLeft = 15;
+      quizEnd();
+}
+}, 1000);
+}
+function quizEnd(){
+  // TA had me try setting quiz to empty string, but it didn't work
+  // questionsEl.innerHTML = "";
+  // choiceOne.innerHTML = "";
+  // choiceTwo.innerHTML = "";
+  // choiceThree.innerHTML = "";
+  // choiceFour.innerHTML = "";
+  if (runningQindex >= lastQindex || secondsLeft === 0){
+    // quiz.style.display = "none";
+    // choiceOne.style.display = "none";
+    
+    // this will show the initials block when the quiz ends
+    intials.style.display = "block";
+}
+
+  var endMessage = document.createElement ("h1");
+  endMessage.textContent = "You are done!";
+  questionsEl.append(endMessage);
+  
+  var inputInitials = document.createElement ("input");
+  inputInitials.setAttribute ('placeholder, type your initials');
+  intials.append(inputInitials);
+
+  var submitBtn = document.createElement("button");
+  submitBtn.textcontent="submit";
+  intials.append(submitBtn);
+
+  submitBtn.addEventListener("click",function(){
+    var user = {
+      name: inputInitials.value,
+      finalScore: correct
     }
-  }, 1000);
-}
+    var storage=JSON.parse(localstorage.getItem('highscore'))
+    if (storage === null){
+      storage = []
+      }
+      storage.push(user)
+      localStorage.setItem('highscore', JSON.stringify(storage))
+        window.location.href = 'highscore.html'
+    
+  })
+
+ }
+// create a second javascript file to show highscores
+
 function sendMessage() {
-  timeEl.textContent = "Your time has burned out!🔥Try again!";
+  timeEl.textContent = "Your time has burned out!🔥";
+  // show score and save to local storage
+  // hide quizz and show up a button to retake- Inside teh new buton fut=ntion do lines 117 and 118
+  secondsLeft = 15;
+  correct = 0;
 }
+
 var startBtn = document.querySelector("#start");
 startBtn.addEventListener("click", startQuiz);
 
 function startQuiz (){
+  // quiz.style.display = "block";
   populateQuestion();
   setTime();
+  
 }
+
+
+// finalScore = correct;
+
+// submitBtn.addEventListener("click",intials)
